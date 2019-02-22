29b7d7876c59fd7b13ff531fb315c1d2a5ae7a47
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-02-22 00:14:45 -0700

    added comment to enum singleton example, added placeholder for serialization singleton example

--- src/main/java/net/jeremiahshore/patterns/creational/singleton/EnumSingleton.java
+++ src/main/java/net/jeremiahshore/patterns/creational/singleton/EnumSingleton.java
@@ -1,6 +1,9 @@
 package net.jeremiahshore.patterns.creational.singleton;
 
-//globally accessible, but does not support lazy initialization
+/*globally accessible, but does not support lazy initialization
+
+also unable to be broken by reflection, as enums don't have a constructor
+ */
 public enum EnumSingleton {
 
     INSTANCE;
---
+++ src/main/java/net/jeremiahshore/patterns/creational/singleton/SerializationProtectedSingleton.java
@@ -0,0 +1,7 @@
+package net.jeremiahshore.patterns.creational.singleton;
+
+public class SerializationProtectedSingleton {
+
+    //todo: create an example to demonstrate how to prevent serialization/de-serialization of a singleton instance
+
+}
